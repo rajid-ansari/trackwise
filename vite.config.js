0bf22841e05a7e1aa9edc4cd4a04d594d6567315
Rajid Ansari <rajidansari25@gmail.com>
2025-04-21 20:27:07 +0530

    v1 prod

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
+    visualizer(),
     // pwa plugin configuration
     VitePWA({
       registerType: 'autoUpdate',

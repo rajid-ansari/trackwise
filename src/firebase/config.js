744f14b48c51e98ab8bdff385f6ea0bc260d9908
Rajid Ansari <rajidansari25@gmail.com>
2025-04-21 20:39:01 +0530

    corrected a typo

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: Import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: Import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: Import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: Import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: Import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: Import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: Import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase with custom settings
